054ecd3e6b5fdac983dbe4182f4001e27aa139fe
Charles <charles@mathcrowd.cn>
2022-01-16 21:30:24 +0800

    add Simple Component for Debug

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import LightsOutPuzzle from './components/LightsOut';
 import LightsOutPuzzlePreview from './components/LightsOutPreview';
+import Hello from './components/SimpleTest';
 
 const App = () => (
   <>
     <LightsOutPuzzle />
     <LightsOutPuzzlePreview />
+    <Hello />
   </>
 );
 
